4f91057d495cc552ed353a663fb39efc5ad602bc
Josevan <josivantarcio@msn.com>
2021-11-24 23:08:52 -0300

    passaTempo3

--- exercicios/src/arrays/Exercicio3.java
+++ exercicios/src/arrays/Exercicio3.java
@@ -10,32 +10,18 @@ public class Exercicio3 {
 
 		System.out.println("Digite a quantidade de alunos: ");
 		int qtdAlunos = scan.nextInt();
-		
+
 		System.out.println("Digite a quantidade de notas: ");
 		int qtdNotas = scan.nextInt();
 
 		double[][] turma = new double[qtdAlunos][qtdNotas];
-		
 
 		for (int i = 0; i < turma.length; i++) {
-			System.out.print("Nome do Aluno: ");
-			String nomeAluno = scan.next();
 			for (int j = 0; j < turma[i].length; j++) {
-				System.out.print("Digite a nota " + (j + 1) + " do Aluno " + nomeAluno + ": ");
+				System.out.print("Digite a nota " + (j + 1) + ": ");
 				turma[i][j] = scan.nextDouble();
 			}
 		}
-
-		for (int i = 0; i < turma.length; i++) {
-			System.out.println(Arrays.toString(turma[i]));
-			double somar = 0;
-			for (int j = 0; j < turma[i].length; j++) {
-				somar += turma[i][j];
-			}
-			System.out.println("Media: "+somar/turma[i].length);
-		}
-		
-		
 
 		scan.close();
 
