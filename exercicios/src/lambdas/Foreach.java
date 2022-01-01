1f82cd1fc8166faff7e7d8487655afa6544cac1a
Josevan <josivantarcio@msn.com>
2022-01-01 17:47:42 -0300

    Method Reference #02
    puxando de um metodo

--- exercicios/src/lambdas/Foreach.java
+++ exercicios/src/lambdas/Foreach.java
@@ -16,7 +16,17 @@ public class Foreach {
 		System.out.println("\nLambda #1");
 		aprovados.forEach(nome -> System.out.println(nome + ";"));
 		
-		System.out.println("\nMethod Reference");
+		System.out.println("\nMethod Reference #1");
 		aprovados.forEach(System.out::println);
+		
+		System.out.println("\nLambda #2");
+		aprovados.forEach(nome -> imprimir(nome));
+		
+		System.out.println("\nMethod Reference #2");
+		aprovados.forEach(Foreach::imprimir);
+	}
+	
+	static void imprimir(String nome) {
+		System.out.println(nome + "!");
 	}
 }
