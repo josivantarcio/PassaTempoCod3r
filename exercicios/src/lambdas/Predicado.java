65eb1cb58dd2aa1fe3a1154cbf0abbe0b5df1388
Josevan <josivantarcio@msn.com>
2022-01-06 00:37:29 -0300

    Update Predicado.java

--- exercicios/src/lambdas/Predicado.java
+++ exercicios/src/lambdas/Predicado.java
@@ -5,10 +5,16 @@ import java.util.function.Predicate;
 public class Predicado {
 	public static void main(String[] args) {
 		
-		Predicate<Produto> registro = casa -> (casa.preco * (1-casa.desconto) > 20.0);
+		Predicate<Produto> registro = casa -> (casa.preco >= 30.00);
 		
 		Produto produto = new Produto("VetMax Pro", 30.00, 0.5);
 		
 		System.out.println(registro.test(produto));
+		
+		if(registro.test(produto)) {
+			System.out.println("É Caro");
+		}else {
+			System.out.println("Não é caro");
+		}
 	}
 }
