18b06d82b4b71c092fc2be941c613ed987c21fce
Josevan <josivantarcio@msn.com>
2022-01-10 21:14:59 -0300

    Update Calculadora4.java
    acrescentando comentários

--- exercicios/src/lambdas/Calculadora4.java
+++ exercicios/src/lambdas/Calculadora4.java
@@ -1,16 +1,34 @@
 package lambdas;
 
+import java.util.Arrays;
+import java.util.List;
 import java.util.function.BinaryOperator;
+import java.util.function.Consumer;
 import java.util.function.Predicate;
 
 public class Calculadora4 {
 	public static void main(String[] args) {
+
+		// Utilizando o modelo reduzido de Lambda.
 		
-		//Utilizando o modelo reduzido de Lambda.
-		
-		BinaryOperator<Double> calc = (x,z) -> x / z;
+		BinaryOperator<Double> calc = (x, z) -> x / z; //BinaryOperator -> recebe dois parametros e não retorna nada.
 		double a = calc.apply(4.0, 2.0);
-		
 		System.out.println(a);
+
+		Consumer<Produto> imprimirLista = p -> System.out.println(p); //Consumer -> recebe um parametro e retorna um.
+		Produto produto = new Produto("Caminhao", 300000.00, 0.10);
+		imprimirLista.accept(produto);
+
+		List<String> listLoja = Arrays.asList("Americanas", "Bahia", "Freitas", "Lupo");
+		listLoja.forEach(System.out::println);
+
+		double valorCaminhao = produto.preco;
+		Predicate<Produto> listProdutos = p -> (p.preco > valorCaminhao); //Predicate -> recebe um parametro e retorna Boolean  
+		if (listProdutos.test(produto)) {
+			System.out.println("Verdadeiro");
+		} else {
+			System.out.println("Falso");
+		}
+
 	}
 }
