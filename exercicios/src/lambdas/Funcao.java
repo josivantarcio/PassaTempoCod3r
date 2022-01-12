a997593d2cbaa4f92d766b68117960cdae130c31
Josevan <josivantarcio@msn.com>
2022-01-12 12:38:41 -0300

    Create Funcao.java

---
+++ exercicios/src/lambdas/Funcao.java
@@ -0,0 +1,14 @@
+package lambdas;
+
+import java.util.function.Function;
+
+public class Funcao {
+	public static void main(String[] args) {
+		Function<Integer, String> numeroPar = num -> num % 2 == 0 ? "Par" : "Impar";
+
+		Function<String, String> mensagem = msn -> "O número é: " + msn;
+		Function<String, String> show = sw -> sw + " !!!" ;
+
+		System.out.println(numeroPar.andThen(mensagem).andThen(show).apply(22));
+	}
+}
