70e0826ce8479300f3bb557ed5669ad493837444
Josevan <josivantarcio@msn.com>
2022-01-14 14:39:43 -0300

    Update Funcao.java

--- exercicios/src/lambdas/Funcao.java
+++ exercicios/src/lambdas/Funcao.java
@@ -5,9 +5,8 @@ import java.util.function.Function;
 public class Funcao {
 	public static void main(String[] args) {
 		Function<Integer, String> numeroPar = num -> num % 2 == 0 ? "Par" : "Impar";
-
 		Function<String, String> mensagem = msn -> "O número é: " + msn;
-		Function<String, String> show = sw -> sw + " !!!" ;
+		Function<String, String> show = sw -> sw + " !!!";
 
 		System.out.println(numeroPar.andThen(mensagem).andThen(show).apply(22));
 	}
