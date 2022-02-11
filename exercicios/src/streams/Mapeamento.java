a966f46706f2289ca1ccc5d99513b09d2d34ae93
Josevan <josivantarcio@msn.com>
2022-02-11 09:24:47 -0300

    Update Mapeamento.java

--- exercicios/src/streams/Mapeamento.java
+++ exercicios/src/streams/Mapeamento.java
@@ -7,11 +7,11 @@ import java.util.function.UnaryOperator;
 
 public class Mapeamento {
 
-	public static final UnaryOperator<String> maiusculo = m -> m.toUpperCase();
+	public final static UnaryOperator<String> maiusculo = m -> m.toUpperCase();
 	
-	public static final UnaryOperator<String> primeiraLetra = pl -> "" + pl.charAt(0);
+	public final static UnaryOperator<String> primeiraLetra = pl -> "" + pl.charAt(0);
 	
-	public static final UnaryOperator<String> grito = g -> g + "!!! ";
+	public final static UnaryOperator<String> grito = g -> g + "!!! ";
 	
 	public final static Consumer<Object> print = System.out::println;
 	
