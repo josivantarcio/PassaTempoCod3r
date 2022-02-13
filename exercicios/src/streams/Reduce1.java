20bc320a77415dc993b2cf97c1a85c6f8bf40204
Josevan <josivantarcio@msn.com>
2022-02-13 09:13:06 -0300

    Exercise #1 (Stream.reduce)
    number one exercise for all classes.
    good luck

--- exercicios/src/streams/Reduce1.java
+++ exercicios/src/streams/Reduce1.java
@@ -3,15 +3,23 @@ package streams;
 import java.util.Arrays;
 import java.util.List;
 import java.util.function.BinaryOperator;
+import java.util.function.UnaryOperator;
 
 public class Reduce1 {
 	public static void main(String[] args) {
 		
 		List<Integer> nums = Arrays.asList(1,2,3,4,5,6,7,8,9);
 		
-		BinaryOperator<Integer> soma = (i, n) -> i + n;
+		BinaryOperator<Integer> soma = (i, n) ->  i+n;
 		
-		System.out.println(nums.stream().reduce(soma));
+		var teste1 = nums.stream().reduce(soma).get();
+		System.out.println(teste1);
 		
+		Integer teste2 = nums.stream()
+			.filter(n -> n%2 == 0)
+			.reduce(soma)
+			.get();
+		
+		System.out.println(teste2);
 	}
 }
