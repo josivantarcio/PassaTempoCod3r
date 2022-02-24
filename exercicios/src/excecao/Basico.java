54c0975c3804f78e2272690655a2d93682bd8852
Josevan Oliveira <josivantarcio@gmail.com>
2022-02-23 21:48:07 -0300

    Update Basico.java

--- exercicios/src/excecao/Basico.java
+++ exercicios/src/excecao/Basico.java
@@ -5,23 +5,25 @@ import streams.Aluno;
 public class Basico {
 	public static void main(String[] args) {
 		Aluno a = null;
-		
+
+		imprimirNomeDoAluno(a);
+		System.out.println("ERRO: Nome do Usuario está em branco");
+
 		try {
-			imprimirNomeDoAluno(a);
-		}catch (Exception e) {
-			System.out.println("ERRO: Nome do Usuario está em branco");
+			System.out.println(7 / 0);
+
+		} catch (ArithmeticException e) {
+			e.getStackTrace();
 		}
-		
-		try {
-			System.out.println(7/0);
-		} catch (Exception e) {
-			System.out.println("Numero nao pode ser divisivel por 0!");
-			
-		}
-		
+		System.out.println("Numero nao pode ser divisivel por 0!");
+
 	}
-	
+
 	public static void imprimirNomeDoAluno(Aluno aluno) {
-		System.out.println(aluno.getName());
+		try {
+			System.out.println(aluno.getName());
+		} catch (Exception e) {
+			e.getMessage();
+		}
 	}
 }
